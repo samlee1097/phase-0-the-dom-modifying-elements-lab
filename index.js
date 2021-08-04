60da45b03fb85c2e741c233b4dfe9253b12782a0
Samuel <samlee1097@yahoo.com>
2021-08-04 17:39:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = "Samuel Lee is the champion";
